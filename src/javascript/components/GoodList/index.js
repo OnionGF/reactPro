8e83deaa709d63027e8d050d09bfa63673595939
GongF <357099265@qq.com>
2018-01-26 17:15:36 +0800

    1.26gf

--- src/javascript/components/GoodList/index.js
+++ src/javascript/components/GoodList/index.js
@@ -16,8 +16,17 @@ class GoodList extends Component {
 
     return (
         <div>
-      
- 
+            <div id='app'>
+                <div className='restaurant-detail'>
+                    <div className='seller-header'>
+                        <div className='content-wrapper'>
+                            
+                        </div>
+                    </div>
+                    <div className='tab'></div>
+                    <div className=''></div>
+                </div>
+            </div>
            商品列表
         </div>
     );
