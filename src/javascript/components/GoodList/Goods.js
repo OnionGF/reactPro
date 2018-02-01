a5caa00ee4758000b5e6954fbee257f457e5b68d
GongF <357099265@qq.com>
2018-02-01 10:49:26 +0800

    2.1gf

--- src/javascript/components/GoodList/Goods.js
+++ src/javascript/components/GoodList/Goods.js
@@ -14,7 +14,19 @@ class Goods extends Component {
 
     return (
         <div>
-          
+            {/* 左右联动 */}
+            <div className="goods">
+                {/* 左侧菜单 */}
+                <div className='menu-wrapper'>
+                
+                </div>
+
+
+                {/* 右侧食物列表 */}
+                <div className='foods-wrapper'>
+                
+                </div>
+            </div>
 
            Goods
         </div>
