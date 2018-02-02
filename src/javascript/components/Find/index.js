d43ac4d71f6e2f255419d9c99cf17ecd0d123a39
swiftlai <2446263627@qq.com>
2018-02-02 10:42:01 +0800

    第二次

--- src/javascript/components/Find/index.js
+++ src/javascript/components/Find/index.js
@@ -1,42 +1,70 @@
 import React, { Component } from 'react';
-
+import { browserHistory } from 'react-router';
 import Footer from '../Footer/Footer.js'
-
+import axios from 'axios'
 class Find extends Component {
     constructor(context,props){
         super(context,props)
         this.state={
-            
+            find:[]
         }
     }
     componentWillMount(){
-      
+     this._findData()
     }
     render() {
+        let {find} = this.state
 
     return (
-        <div className="whole">
-            <div className="bigbox">
+        
+    <div className="find">     
+
+        {
+               find.map((data,i)=>(
+
+
+        <div className="whole" key={i}> 
+
+            <div className="bigbox" >
                 
                 <dl>
                      <dt>
-                         <img src="https://p1.meituan.net/gpa/c5f5a4ea35d8a6ab77815efb94fc0722100193.jpg%40720w_348h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D2%26y%3D2%26relative%3D1%26o%3D20" alt=""/>
+                         <img src={data.img} alt=""/>
                      </dt>
-                     <dd className="ddd">太子妃吹出好莱坞效果！明星扎堆的影视城</dd>
+                     <dd className="ddd">{data.title}</dd>
                      <div className="life">
                             周边生活
                      </div>
                      <div className="num">
-                         11111人看过
+                         {data.view}
                      </div>
                 </dl>
                 
             </div>
 
-        
         </div>
+       
+               
+               ))          
+        }   
+         </div>    
     )
   }
+    _findData (){
+        axios.get('/api/findList').then(res=>{
+            console.log(res)
+            if(res.data.code === 0){
+                this.setState(
+                this.state.find = res.data.data.data
+                )
+            }
+        }).catch(err =>{
+            console.log(err)
+        })
+        console.log()
+        }
+   
+
 }
 
 export default Find;
